8eeaa477ecef4466b2865d9c4b8f0de5311356a8
nik-agarwal <67484380+nik-agarwal@users.noreply.github.com>
2023-07-14 12:30:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const port = 8000;
+
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+const Users = require('./models/user');
+const Faqs = require('./models/faqs');
+const faqController = require("./controllers/faq_controller");
+
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+// const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+const MongoStore = require('connect-mongo')(session);
+
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(session({
+    name: 'codial',
+    // TODO change the secret before deployment 
+    secret: 'something',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        // No. in miliseconds
+        maxAge: (1000*60*100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        }, function(err){
+        console.log(err || 'connect-mongodb setup ok');
+    })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use('/', require('./routes'));
+
+app.listen(process.env.PORT, function(err){
+    if(err){
+        console.log(`Error in running the server ${err}`);
+    }
+    console.log(`Listening on port ${process.env.PORT}`);
+});
